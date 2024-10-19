efb8185ee624b335f31c0c9dc06298ca6238fe1f
tonny-lec <tn4646.btnq@gmail.com>
2024-10-19 10:08:29 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# backend フォルダで
+
+```
+cd backend
+mvn spring-boot:run
+```
+
+# frontend フォルダで
+
+```
+cd frontend
+npm install
+npm start
+```
---
+++ backend/src/test/java/jp/focus/line/calm/CalmLineFocusApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.focus.line.calm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmLineFocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
