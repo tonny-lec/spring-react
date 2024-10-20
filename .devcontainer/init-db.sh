323116c6233dd79c7851da736c48323c63737c6e
tonny-lec <tn4646.btnq@gmail.com>
2024-10-20 13:16:20 +0000

    chore: install postgreSQL

--- .devcontainer/init-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-psql -U postgres -c "CREATE DATABASE calmline;"
-psql -U postgres -c "CREATE USER dev WITH ENCRYPTED PASSWORD 'devpassword';"
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE calmline TO dev;"
---
+++ scripts/backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# PostgreSQLコンテナからデータベースのダンプを取得
+# docker exec postgres-db pg_dump -U postgres mydb > mydb_dump_$(date +"%Y%m%d%H%M%S").sql
+# echo "Database dumped to mydb_dump_$(date +"%Y%m%d%H%M%S").sql"
+
+docker exec -t postgres_db pg_dump -U user mydatabase > ../db_data/dump.sql
