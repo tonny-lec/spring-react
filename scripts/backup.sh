d9723ccdd9694edf6a2e04cbec85896300c84f11
tonny-lec <tn4646.btnq@gmail.com>
2024-10-26 09:26:26 +0900

    chore: fix shell

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# PostgreSQLコンテナからデータベースのダンプを取得
-# docker exec postgres-db pg_dump -U postgres mydb > mydb_dump_$(date +"%Y%m%d%H%M%S").sql
-# echo "Database dumped to mydb_dump_$(date +"%Y%m%d%H%M%S").sql"
-
 docker exec -t postgres-db pg_dump -U user mydatabase > ../db_data/dump.sql
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# # リストアするSQLファイルを指定
-# if [ $# -eq 0 ]; then
-#   echo "Usage: $0 <dump_file.sql>"
-#   exit 1
-# fi
-
-# # PostgreSQLコンテナにリストア
-# docker exec -i postgres-db psql -U postgres mydb < "$1"
-# echo "Database restored from $1"
-
-docker exec -i postgres_db psql -U user mydatabase < ../db_data/dump.sql
+docker exec -i postgres-db psql -U user mydatabase < ../db_data/dump.sql
