f68f197751396a35064756591776ca7f2cae9149
tonny-lec <tn4646.btnq@gmail.com>
2024-10-26 12:20:38 +0900

    chore: add echo system date and do not print tar log

--- db_container_bk.sh
+++ db_container_bk.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
+DATE=$(date +%Y%m%d_%H%M%S)
+
 # .env ファイルを読み込み
 if [ -f .env ]; then
   source .env
 else
-  echo ".env ファイルが見つかりません"
+  echo "$DATE .env ファイルが見つかりません"
   exit 1
 fi
 
 # 日付付きのバックアップファイル名
-DATE=$(date +%Y%m%d_%H%M%S)
 BACKUP_FILE="db_backup_$DATE.tar.gz"
 
 # 権限エラーに備えてsudoでバックアップ実行
-sudo tar -czvf "$BACKUP_DIR/$BACKUP_FILE" "$TARGET_DIR"
+sudo tar -czf "$BACKUP_DIR/$BACKUP_FILE" "$TARGET_DIR"
 
 # 完了メッセージ
 if [ $? -eq 0 ]; then
-  echo "Backup succeeded: $BACKUP_FILE"
+  echo "$DATE Backup succeeded: $BACKUP_FILE"
 else
-  echo "Backup failed."
+  echo "$DATE Backup failed."
   exit 1  # 失敗した場合、ここで終了 
 fi
 
 # 14 日以上経過した古いバックアップを削除
 find "$BACKUP_DIR" -type f -name "*.tar.gz" -mtime +14 -exec rm {} \;
 
-echo "Old backups cleaned up."
+echo "$DATE Old backups cleaned up."
--- db_restore.sh
+++ db_restore.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DATE=$(date +%Y%m%d_%H%M%S)
+
 # .env ファイルを読み込み
 if [ -f .env ]; then
   source .env
 else
-  echo ".env ファイルが見つかりません"
+  echo "$DATE .env ファイルが見つかりません"
   exit 1
 fi
 
@@ -13,25 +15,25 @@ LATEST_BACKUP=$(ls -t $BACKUP_DIR/*.tar.gz | head -n 1)
 
 # 1. バックアップファイルの確認
 if [ -z "$LATEST_BACKUP" ]; then
-  echo "No backup file found in $BACKUP_DIR."
+  echo "$DATE No backup file found in $BACKUP_DIR."
   exit 1  # バックアップファイルがない場合、終了
 fi
 
-echo "Restoring from backup: $LATEST_BACKUP"
+echo "$DATE Restoring from backup: $LATEST_BACKUP"
 
 # 2. PostgreSQLコンテナの停止 (必要に応じて変更)
 docker stop calmline-focus_devcontainer-db-1
 
 # 3. 既存のデータを削除
 sudo rm -rf "$TARGET_DIR"/*
-echo "Existing data in $TARGET_DIR has been cleared."
+echo "$DATE Existing data in $TARGET_DIR has been cleared."
 
 # 4. バックアップファイルを解凍してリストア
-sudo tar -xzvf "$LATEST_BACKUP" -C /
-echo "Backup restored to $TARGET_DIR."
+sudo tar -xzf "$LATEST_BACKUP" -C /
+echo "$DATE Backup restored to $TARGET_DIR."
 
 # 5. PostgreSQLコンテナの再起動
 docker start calmline-focus_devcontainer-db-1
-echo "PostgreSQL container has been restarted."
+echo "$DATE PostgreSQL container has been restarted."
 
-echo "Restore completed successfully."
+echo "$DATE Restore completed successfully."
